c5422a7546f1bd43f7d0b5c366d335867b8e8ee7
renzo <renzo_or@hotmail.com>
2019-01-09 10:32:46 -0500

    primer commit

---
+++ gettingstarted/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+
+urlpatterns = [
+    # Paths del core
+    path('', include('core.urls')),
+    # Paths de services
+    path('services/', include('services.urls')),
+    # Paths de blog
+    path('blog/', include('blog.urls')),
+    # Paths de pages
+    path('page/', include('pages.urls')),
+    # Paths de pages
+    path('contact/', include('contact.urls')),
+    # Paths del admin
+    path('admin/', admin.site.urls),
+]
+
